6d59bd0211a9d91004975dde419e0ed4cef9de5c
Donnie <122802443+Dnnavant@users.noreply.github.com>
2024-07-06 07:17:04 -0400

    removed hamburger icon from app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,12 @@ import HomeSection from "./Components/HomeSection/HomeSection";
 import Navbar from "./Components/Navbar/Navbar";
 import ProjectsSection from "./Components/ProjectsSection/ProjectsSection";
 import ProfileCard from "./Components/ProfileCard/ProfileCard";
-import HamBurgerIcon from "./Components/HamBurgerIcon/HamBurgerIcon";
+
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <HamBurgerIcon /> */}
       
       <HomeSection />
       {/* <Introduction /> */}

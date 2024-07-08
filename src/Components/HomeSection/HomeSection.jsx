4cfb4ef5534fafb3fb117fab79708dc6450c8462
Donnie <122802443+Dnnavant@users.noreply.github.com>
2024-07-07 21:14:02 -0400

    updated Home section with title and alignment of title

--- src/Components/HomeSection/HomeSection.jsx
+++ src/Components/HomeSection/HomeSection.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import "./HomeSection.css";
 
 export default function HomeSection() {
-  return (
-    <div>
-      {/* Main Body Content */}
-      <article className="titleContainer">
-        <span>
-        <a href="#home">
-          <img
-            className="navbar-logo"
-            src="./images/Web-Dev-Logo.svg"
-            alt="DA-web-dev logo"
-          />
-          <h2>
-          Dev Designs
-        </h2>
-        </a>
-        <h3 className="subTitle">
-          Creativity Is Limited Only If You Want It To Be!
-        </h3></span>
-      </article>
-    </div>
-  );
+	return (
+		<div className="HS-Container">
+			{/* Main Body Content */}
+			
+				<div className="HS-logoContainer">
+					<div className="HS-logo">
+						<a href="#home">
+							<img
+								className="DA-web-dev-logo"
+								src="./images/Web-Dev-Logo.svg"
+								alt="DA-web-dev logo"
+							/>
+						</a>
+					</div>
+							<h2 className="HS-title">Dev Designs</h2>
+				</div>
+				{/* <div className="HS-subTitle">
+					<h3>
+						Creativity Is Limited Only If You Want It To Be!
+					</h3>
+				</div> */}
+		</div>
+	);
 }
